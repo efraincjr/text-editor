549b7fb15c9b2698878bbae197c24dc1b3c96af9
Efrain Castro Jr <efraincastrojrGmail.com>
2023-01-23 12:31:53 -0600

    install js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -7,16 +7,24 @@ window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   // stash the event so it can be triggered later
   window.deferredPrompt = event;
-  // remove the 'hidden' class from the install button container
+  // removes the 'hidden' class from the install button container
   butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      return;
+    }
+    // show the install prompt
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    // hide the install button
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    
+    window.deferredPrompt = null;
 });
